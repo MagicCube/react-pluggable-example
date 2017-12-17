2ed3148ccdabceade6588af3d4f4e4198dbd583d
Henry Li <henry1943@163.com>
2017-12-17 20:37:39 +0800

    Connect App with store

--- src/im/components/App/index.jsx
+++ src/im/components/App/index.jsx
@@ -1,12 +1,28 @@
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import React, { PureComponent } from 'react';
+
+import MessageList from '../MessageList';
 
 import styles from './index.less';
 
+@connect(state => ({ messages: state.session.messages }))
 export default class App extends PureComponent {
+  static propTypes = {
+    messages: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number
+    }))
+  }
+
+  static defaultProps = {
+    messages: []
+  }
+
   render() {
+    const { messages } = this.props;
     return (
       <div className={styles.container}>
-        App
+        <MessageList messages={messages} />
       </div>
     );
   }
