223c067c52bfa2812a2a5244545007b094d753f2
Henry Li <henry1943@163.com>
2017-12-17 19:31:26 +0800

    Add index.jsx

---
+++ src/im/components/App/index.jsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+import styles from './index.less';
+
+export default class App extends PureComponent {
+  render() {
+    return (
+      <div className={styles.container}>
+        App
+      </div>
+    );
+  }
+}
