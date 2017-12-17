85c9c2850aecdc80ebd5f070eb26a08af9cdb3f3
Henry Li <henry1943@163.com>
2017-12-17 20:39:13 +0800

    Add index.jsx

---
+++ src/plugins/messages/ImageMessage/index.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import React, { PureComponent } from 'react';
+
+import styles from './index.less';
+
+export default class PictureMessage extends PureComponent {
+  static propTypes = {
+    thumbnailUrl: PropTypes.string
+  }
+
+  static defaultProps = {
+    thumbnailUrl: null
+  }
+
+  render() {
+    const { thumbnailUrl } = this.props;
+    return (
+      <div className={styles.container}>
+        {thumbnailUrl}
+      </div>
+    );
+  }
+}
