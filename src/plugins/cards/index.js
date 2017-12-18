658fa5bb4eb5d8b5cbbe4b38af43af5464f027b7
Henry Li <henry1943@163.com>
2017-12-18 10:30:06 +0800

    Update index.js

--- src/plugins/cards/index.js
+++ src/plugins/cards/index.js
@@ -2,6 +2,6 @@ import { registerCardClass } from '../../im/plugin';
 import LuckyMoneyCard from './LuckyMoneyCard';
 import WebLinkCard from './WebLinkCard';
 
-registerCardClass('com.example.plugins.cards.LuckyMoneyCard', LuckyMoneyCard);
-registerCardClass('com.example.plugins.cards.WebLinkCard', WebLinkCard);
+registerCardClass('com.example.LuckyMoneyCard', LuckyMoneyCard);
+registerCardClass('com.example.WebLinkCard', WebLinkCard);
 
